883544f9afbea7c72fa5be11aeeeea420e512fbe
SnorlaxSE <19644938+SnorlaxSE@users.noreply.github.com>
2020-08-03 20:45:12 +0800

    Update bat

--- 1-Installation-conda.bat
+++ 1-Installation-conda.bat
@@ -15,9 +15,4 @@ CALL conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/c
 
 CALL conda config --set show_channel_urls yes
 
-conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
-conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge 
-conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/msys2/
-conda config --set show_channel_urls yes
-
 pause
